bbdcd8a62028d0bd2f207be17285f92cf676b75d
Davo43 <david.rodriguez2@campus.xaloc.org>
2026-09-18 09:41:43 +0200

    Prueba 1 guiada

---
+++ print hola.py
@@ -0,0 +1,54 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+======================================================= RESTART: C:/Users/RodríguezHomsDavid/Downloads/print hola.py =======================================================
+hola
+
+======================================================== RESTART: C:\Users\RodríguezHomsDavid\Desktop\print hola.py ========================================================
+hola
+adiós
+>>> 
+======================================================== RESTART: C:\Users\RodríguezHomsDavid\Desktop\print hola.py ========================================================
+hola
+adiós
+104
+>>> 
+=========== RESTART: C:\Users\RodríguezHomsDavid\Desktop\print hola.py ===========
+variable1
+variable2
+>>> 
+=========== RESTART: C:\Users\RodríguezHomsDavid\Desktop\print hola.py ===========
+hola
+adiós
+>>> 
+=========== RESTART: C:\Users\RodríguezHomsDavid\Desktop\print hola.py ===========
+Traceback (most recent call last):
+  File "C:\Users\RodríguezHomsDavid\Desktop\print hola.py", line 1, in <module>
+    variable1=imput("introduce el primer número:")
+NameError: name 'imput' is not defined. Did you mean: 'input'?
+>>> 
+=========== RESTART: C:\Users\RodríguezHomsDavid\Desktop\print hola.py ===========
+introduce el primer número:3
+imtroduce el segundo número:4
+34
+>>> 
+=========== RESTART: C:\Users\RodríguezHomsDavid\Desktop\print hola.py ===========
+introduce el primer número:2
+imtroduce el segundo número:4
+24
+hola
+>>> 
+=========== RESTART: C:\Users\RodríguezHomsDavid\Desktop\print hola.py ===========
+introduce el primer número:4
+imtroduce el segundo número:2
+la suma es:  42
+>>> 
+=========== RESTART: C:\Users\RodríguezHomsDavid\Desktop\print hola.py ===========
+introduce el primer número:2
+imtroduce el segundo número:2
+la suma es:  4
+>>> 
+=========== RESTART: C:\Users\RodríguezHomsDavid\Desktop\print hola.py ===========
+introduce el primer número:2
+imtroduce el segundo número:2
+la suma es:  4
